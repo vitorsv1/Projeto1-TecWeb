749aa43ee24d0d61513b1f42c1ce00e4d7e5497b
IagoMendes <iagorm@al.insper.edu.br>
2018-09-10 13:27:23 -0300

    organização dao

--- Teepo/src/br/edu/insper/DAO.java
+++ Teepo/src/br/edu/insper/DAO.java
@@ -22,32 +22,7 @@ public class DAO {
 			e.printStackTrace();
 		}
 	}
-	public List<Notas> getNotas(){          //Pega todas as Notas do database
-		List<Notas> Notas = new ArrayList<Notas>();
-		
-		PreparedStatement stmt;
-		try {
-			stmt = connection.
-					prepareStatement("SELECT * FROM notas");
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				Notas nota = new Notas();
-				nota.setIdNota(rs.getInt("idNota"));
-				nota.setIdCategoria(rs.getInt("idCategoria"));
-				nota.setConteudo(rs.getString("conteudo"));
-				
-				Notas.add(nota);
-				}
-			rs.close();
-			stmt.close();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return Notas;
-		
-	}
-
+	
 	public List<Categorias> getCategorias(){          //Pega todas as Categorias do database
 		List<Categorias> Categorias = new ArrayList<Categorias>();
 		
@@ -73,6 +48,61 @@ public class DAO {
 		
 	}
 	
+	public void adicionaCategoria(Categorias categ) {           //adiciona/edita Categorias
+			
+			String sql = "INSERT INTO categorias" +
+						 "(titulo) values(?)";
+			PreparedStatement stmt;
+			System.out.println(categ.getTitulo());
+			try {
+				stmt = connection.prepareStatement(sql);
+				stmt.setString(1, categ.getTitulo());
+				stmt.execute();
+				stmt.close();
+			} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+	
+	public void removeCategoria(Integer id) {				//remove categoria do id x
+		PreparedStatement stmt;
+		try {
+			stmt = connection
+			 .prepareStatement("DELETE FROM categorias WHERE idCategoria=?");
+			stmt.setLong(1, id);
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			}
+		}
+	
+	public List<Notas> getNotas(){          //Pega todas as Notas do database
+		List<Notas> Notas = new ArrayList<Notas>();
+		
+		PreparedStatement stmt;
+		try {
+			stmt = connection.
+					prepareStatement("SELECT * FROM notas");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Notas nota = new Notas();
+				nota.setIdNota(rs.getInt("idNota"));
+				nota.setIdCategoria(rs.getInt("idCategoria"));
+				nota.setConteudo(rs.getString("conteudo"));
+				
+				Notas.add(nota);
+				}
+			rs.close();
+			stmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return Notas;
+		
+	}
+	
 	public void adicionaNota(Notas nota) {           //adiciona/edita Notas (para editar temos que adicionar a nota no id antigo)
 												     //se não funcionar, ver o edita do handout
 		String sql = "INSERT INTO notas" +
@@ -88,29 +118,13 @@ public class DAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 			}
-		}
-	
-	public void adicionaCategoria(Categorias categ) {           //adiciona/edita Categorias
-		
-		String sql = "INSERT INTO categorias" +
-					 "(titulo) values(?)";
-		PreparedStatement stmt;
-		System.out.println(categ.getTitulo());
-		try {
-			stmt = connection.prepareStatement(sql);
-			stmt.setString(1, categ.getTitulo());
-			stmt.execute();
-			stmt.close();
-		} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		}	
 
-	public void remove(Integer id) {				//remove nota do id x
+	public void removeNota(Integer id) {				//remove nota do id x
 		PreparedStatement stmt;
 		try {
 			stmt = connection
-			 .prepareStatement("DELETE FROM notas WHERE id=?");
+			 .prepareStatement("DELETE FROM notas WHERE idNota=?");
 			stmt.setLong(1, id);
 			stmt.execute();
 			stmt.close();
