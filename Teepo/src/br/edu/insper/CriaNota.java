de96df6e14fd02dd10d1b37a9e49fb1747706c93
vitorsv1 <vitorsv1@al.insper.edu.br>
2018-09-11 08:33:02 -0300

    Criando JSPs e Servlets para Notas

--- Teepo/src/br/edu/insper/CriaNota.java
+++ Teepo/src/br/edu/insper/CriaNota.java
@@ -4,30 +4,26 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/criaNota")  // Falta adicionar nota na categoria selecionada
+@WebServlet("/criaNota")
 public class CriaNota extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	public CriaNota() {
+		super();
+	}
+	
 	@Override
-	protected void doGet(HttpServletRequest request,
-						 HttpServletResponse response)
-						 throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("<form method='post'>");
-		out.println("Conteudo: <input type='text' name='conteudo'><br>");
-		//out.println("Categoria: <input type='text' name='titulo'><br>");
-		out.println("<input type='submit' value='Submit'>");
-		out.println("</form>");
-		out.println("</body></html>");
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	 }
 	
 	@Override
-	protected void doPost(HttpServletRequest request,
-					      HttpServletResponse response)
-					      throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		DAO dao = new DAO();
 		 
 		Notas nota = new Notas();
@@ -37,11 +33,7 @@ public class CriaNota extends HttpServlet {
 		//categ.getIdCategoria();
 		
 		dao.adicionaNota(nota);
-		 
-		 //PrintWriter out = response.getWriter();
-		 //out.println("<html><body>");
-		 //out.println("adicionado" + nota.getConteudo() + "na Categoria" + categ.getTitulo());
-		 //out.println("</body></html>");		 
-		 dao.close();
-}
+		 	 
+		dao.close();
+	}
 }
---
+++ Teepo/src/br/edu/insper/RemoveNota.java
@@ -0,0 +1,44 @@
+package br.edu.insper;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RemoveNota
+ */
+@WebServlet("/RemoveNota")
+public class RemoveNota extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RemoveNota() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
